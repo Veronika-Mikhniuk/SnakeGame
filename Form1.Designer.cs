d8b8f8784e6de8cc0e87a2791f93e04bb24b0f9e
Veronika.Mikhnyuk <Veronika.Mikhnyuk@BY-PF2DXKQV>
2022-09-06 13:05:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace SnakeGame
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbBestScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbScore = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(945, 567);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbBestScore
+            // 
+            this.lbBestScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbBestScore.AutoSize = true;
+            this.lbBestScore.BackColor = System.Drawing.Color.Transparent;
+            this.lbBestScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbBestScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbBestScore.Location = new System.Drawing.Point(891, 617);
+            this.lbBestScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbBestScore.Name = "lbBestScore";
+            this.lbBestScore.Size = new System.Drawing.Size(62, 32);
+            this.lbBestScore.TabIndex = 8;
+            this.lbBestScore.Text = "100";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(689, 617);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "BEST SCORE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(16, 617);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "SCORE:";
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.BackColor = System.Drawing.Color.Transparent;
+            this.lbScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbScore.Location = new System.Drawing.Point(137, 618);
+            this.lbScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(30, 32);
+            this.lbScore.TabIndex = 5;
+            this.lbScore.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel1.Controls.Add(this.btnNewGame);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(207, 202);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(547, 194);
+            this.panel1.TabIndex = 9;
+            this.panel1.Visible = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNewGame.ForeColor = System.Drawing.Color.White;
+            this.btnNewGame.Location = new System.Drawing.Point(145, 107);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(231, 50);
+            this.btnNewGame.TabIndex = 5;
+            this.btnNewGame.Text = "NEW GAME";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(61, 21);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(410, 69);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "GAME OVER!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(977, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbBestScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label lbBestScore;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lbScore;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnNewGame;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+	public partial class Form1 : Form
+	{
+		Point[] snake;
+		Point apple;
+		Random r = new Random();
+		SolidBrush colorOfGameField;
+		SolidBrush colorOfSnake;
+		SolidBrush colorOfApple;
+		string direction;
+		int lenghtOfSnake;
+		int widthOfPlayingField;
+		int heightOfPlayingField;
+
+		private void InstanceSnake()
+		{
+			snake = new Point[10000];
+			lenghtOfSnake = 5;
+			direction = "Up";
+			lbScore.Text = "0";
+			timer1.Interval = 100;
+
+			for (int i = 0; i < 5; i++)
+			{
+				snake[i].X = widthOfPlayingField / 2;
+				snake[i].Y = heightOfPlayingField / 2+i;
+			}
+
+			apple.X = r.Next(0, widthOfPlayingField - 1);
+			apple.Y = r.Next(0, heightOfPlayingField - 1);
+		}
+		public Form1()//Конструктор
+		{
+			InitializeComponent();
+			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			widthOfPlayingField = pictureBox1.Width / 10;
+			heightOfPlayingField = pictureBox1.Height / 10;
+			
+			InstanceSnake();
+
+
+
+			colorOfGameField = new SolidBrush(Color.Black);
+			colorOfSnake = new SolidBrush(Color.DarkRed);
+			colorOfApple = new SolidBrush(Color.Green);
+			//apple.X = r.Next(0, widthOfPlayingField-1);
+			//apple.Y = r.Next(0, heightOfPlayingField - 1);
+
+		}
+
+		private void CheckGameOver()
+		{
+			for (int i = 1; i < lenghtOfSnake; i++)
+			{
+				if (snake[0].X == snake[i].X && snake[0].Y == snake[i].Y)
+				{
+					timer1.Stop();
+					panel1.Visible = true;
+				}
+
+			}
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			Graphics gamePanel = Graphics.FromImage(pictureBox1.Image);
+			gamePanel.FillRectangle(colorOfGameField, 0, 0, pictureBox1.Width, pictureBox1.Height);
+			
+			CheckGameOver();
+
+
+
+			if (lenghtOfSnake > 10000 - 3) lenghtOfSnake = 10000 - 3;//Поверка выхода за пределы массива
+			for (int i = lenghtOfSnake; i >= 0; i--)
+			{
+				snake[i + 1].X = snake[i].X;//Предыдущую координату записываем в следующую
+				snake[i + 1].Y = snake[i].Y;
+			}
+
+
+			if (direction == "Up") snake[0].Y -= 1;
+			if (direction == "Down") snake[0].Y += 1;
+			if (direction == "Right") snake[0].X += 1;
+			if (direction == "Left") snake[0].X -= 1;
+
+			for (int i = 0; i < lenghtOfSnake; i++)
+			{
+				//Проход сквозь стены	
+				if (snake[i].X < 0) snake[i].X = snake[i].X + widthOfPlayingField;
+				if (snake[i].X > widthOfPlayingField) snake[i].X = 0;
+				if (snake[i].Y < 0) snake[i].Y = snake[i].Y + heightOfPlayingField;
+				if (snake[i].Y > heightOfPlayingField) snake[i].Y = 0;
+
+				gamePanel.FillEllipse(colorOfSnake, snake[i].X * 10, snake[i].Y * 10, 10, 10);
+				if (apple.X == snake[0].X && apple.Y == snake[0].Y)//Рос змеи при стедани яблока
+				{
+					apple.X = r.Next(0, widthOfPlayingField - 1);
+					apple.Y = r.Next(0, heightOfPlayingField - 1);
+					lenghtOfSnake++;
+					timer1.Interval = timer1.Interval == 30 ? timer1.Interval : timer1.Interval - 1;
+					lbScore.Text = (int.Parse(lbScore.Text) + 1).ToString();
+				}
+			}
+			for (int i = 1; i < lenghtOfSnake; i++)
+			{
+
+				if (apple.X == snake[i].X && apple.Y == snake[i].Y)//Проверка появления яблока на теле змеи
+				{
+					apple.X = r.Next(0, widthOfPlayingField - 1);
+					apple.Y = r.Next(0, heightOfPlayingField - 1);
+				}
+			}
+
+			gamePanel.FillEllipse(colorOfApple, apple.X * 10, apple.Y * 10, 10, 10);
+
+
+
+			//if (lenghtOfSnake < 6) lenghtOfSnake++;
+
+			pictureBox1.Invalidate();
+
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			direction = e.KeyCode == Keys.Left && direction != "Right" ? "Left" : direction;
+			direction = e.KeyCode == Keys.Right && direction != "Left" ? "Right" : direction;
+			direction = e.KeyCode == Keys.Up && direction != "Down" ? "Up" : direction;
+			direction = e.KeyCode == Keys.Down && direction != "Up" ? "Down" : direction;
+		}
+
+		private void btnNewGame_Click(object sender, EventArgs e)
+		{
+			panel1.Visible = false;
+			InstanceSnake();
+			timer1.Start();
+		}
+	}
+}
